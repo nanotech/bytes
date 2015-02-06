9c6d916e31c17b42c12045aa6a90eb2766c32351
NanoTech <nanotech@nanotechcorp.net>
2015-02-06 03:09:51 -0600

    C++ compatibility

--- bytes.h
+++ bytes.h
@@ -21,11 +21,11 @@ typedef struct bytes {
     size_t BYTES_INTERNAL_FIELD(length);
 } bytes;
 
-#define bytes_construct_unsafe(DATA, LEN)     \
-    ((bytes){                                 \
-        .BYTES_INTERNAL_FIELD(data) = (DATA), \
-        .BYTES_INTERNAL_FIELD(length) = (LEN) \
-    })
+#ifdef __cplusplus
+#define bytes_construct_unsafe(DATA, LEN) (bytes{ (DATA), (LEN) })
+#else
+#define bytes_construct_unsafe(DATA, LEN) ((bytes){ (DATA), (LEN) })
+#endif
 
 // Constructors {{{
 
@@ -44,11 +44,11 @@ static inline struct bytes bytes_alloc(void *(*alloc)(size_t), size_t length) {
 // }}}
 
 static inline uint8_t *bytes_mutable_data(struct bytes b) {
-    return b.BYTES_INTERNAL_FIELD(data);
+    return (uint8_t *)b.BYTES_INTERNAL_FIELD(data);
 }
 
 static inline const uint8_t *bytes_data(struct bytes b) {
-    return b.BYTES_INTERNAL_FIELD(data);
+    return (const uint8_t *)b.BYTES_INTERNAL_FIELD(data);
 }
 
 static inline size_t bytes_length(struct bytes b) {
@@ -120,10 +120,8 @@ typedef struct bytes_builder {
 } bytes_builder;
 
 static inline void bytes_builder_init(struct bytes_builder *bb, struct bytes b) {
-    *bb = (struct bytes_builder){
-        .BYTES_INTERNAL_FIELD(target) = b,
-        .BYTES_INTERNAL_FIELD(offset) = 0,
-    };
+    bb->BYTES_INTERNAL_FIELD(target) = b;
+    bb->BYTES_INTERNAL_FIELD(offset) = 0;
 }
 
 static inline bool bytes_builder_append(struct bytes_builder *bb, struct bytes b) {
@@ -152,10 +150,10 @@ typedef struct bytes_buffer {
 } bytes_buffer;
 
 static inline struct bytes_buffer bytes_buffer_from(struct bytes b) {
-    return (struct bytes_buffer){
-        .bytes = b,
-        .BYTES_INTERNAL_FIELD(original) = b,
-    };
+    struct bytes_buffer bb;
+    bb.bytes = b;
+    bb.BYTES_INTERNAL_FIELD(original) = b;
+    return bb;
 }
 
 static inline void bytes_buffer_reset(struct bytes_buffer *bb) {
